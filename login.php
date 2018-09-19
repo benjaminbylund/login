2c463133c192a7ab1908e705bef3d836ebc0d486
benjaminbylund <te16.benjamin.bylund@umea.nti.se>
2018-09-19 10:47:04 +0200

    gjort så det inte funkar

--- login.php
+++ login.php
@@ -1,6 +1,27 @@
 <?php
+session_start();
+if(isset($_POST['sumbit'])){
 
+$loginCredentials = [
+"username" => "benjamin", 
+"password" => "123"
+];
 
-echo "<pre>" . var_dump($_POST) . "</pre>";
+if (isset($_POST['username']) && isset($_POST['password'])) {
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	$password = md5($_POST['password']);
+
+	echo $password;
+
+	if($username == $loginCredentials['username']
+		&& $password == md5($loginCredentials['password'])){
+		echo "<h1> hello my man </h1>";
+	}
+	elseif ($username != $loginCredentials['username']
+		|| $password != $loginCredentials['password']){
+		echo "<h1> DU HAR FEL</h1>";
+		}
+	}
+}
 
 ?>
