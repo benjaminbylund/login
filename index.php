d93d3a7b2b245810a582593379a0a67335c967c3
benjaminbylund <te16.benjamin.bylund@umea.nti.se>
2018-09-19 11:15:32 +0200

    Nu funkar det typ

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
 	<link href="https://fonts.googleapis.com/css?family=New+Rocker" rel="stylesheet">
 	<title>Shh hemligt</title>
 	<style type="text/css">
+		html{
+			background-image: url(bg.jpg);
+			background-size: 95%;
+			background-repeat: true;
+		}
 		main{
 			width: 80%;
 			margin: 0px auto;
@@ -13,6 +18,7 @@
 		body{
 			font-family: 'New Rocker', cursive;
 		}
+
 	</style>
 </head>
 <body>
@@ -23,11 +29,11 @@
 			<legend>Logga in</legend>
 			<p>
 				<label for="username">Användernamn: </label>
-				<input type="text" name="username" id="username">
+				<input type="text" name="username" id="username" autocomplete="off">
 			</p>
 			<p>
 				<label for="password">Lösenord: </label>
-				<input type="password" name="passowrd" id="password">
+				<input type="password" name="password" id="password" autocomplete="off">
 			</p>
 			<p>
 				<input type="submit" name="submit" id=submit value="Logga in">
--- login.php
+++ login.php
@@ -1,27 +1,45 @@
 <?php
 session_start();
-if(isset($_POST['sumbit'])){
+if(isset($_POST['submit'])){
 
-$loginCredentials = [
-"username" => "benjamin", 
-"password" => "123"
-];
+	$loginCredentials = [
+		"username" => "benjamin", 
+		"password" => password_hash("123", PASSWORD_DEFAULT)
+	];
+	echo $loginCredentials['password'];
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
-	$password = md5($_POST['password']);
+	if (isset($_POST['username']) && isset($_POST['password'])) 
+	{
+		$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+		$password = $_POST['password'];
 
-	echo $password;
-
-	if($username == $loginCredentials['username']
-		&& $password == md5($loginCredentials['password'])){
-		echo "<h1> hello my man </h1>";
-	}
-	elseif ($username != $loginCredentials['username']
-		|| $password != $loginCredentials['password']){
-		echo "<h1> DU HAR FEL</h1>";
+		if($username == $loginCredentials['username']
+			&& password_verfiy($password, $loginCredentials['password']))
+		{
+			echo "<h1> hello my man </h1>";
+		}
+		elseif ($username != $loginCredentials['username']
+			|| $password != $loginCredentials['password'])
+		{
+			echo "<h1> DU HAR FEL</h1>";
+		}
+		else
+		{
+			echo "<h1>FEEEL</h1>";
 		}
 	}
 }
+else
+{
+	echo "<h1> VARFÖR ÄR DU HÄR? </h1>";
+}
 
 ?>
+
+
+
+
+
+
+
+		
