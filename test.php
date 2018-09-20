aa1e5097fa63d6a2d27435fab4198c76d14d703b
benjaminbylund <te16.benjamin.bylund@umea.nti.se>
2018-09-20 10:47:24 +0200

    Förbättrade test

--- test.php
+++ test.php
@@ -2,6 +2,10 @@
 include 'db.php';
 
 
+foreach ($dbh->query('SELECT * FROM login') as $row {
+	print_r($row);
+}
+
 $statement = $dbh->query("SELECT * FROM login");
 $row = $statement->fetch(PDO::FETCH_ASSOC);
 
