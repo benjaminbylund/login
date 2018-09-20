bcb21e93bb6fbe30c79b6f257379631d522bfd8b
benjaminbylund <te16.benjamin.bylund@umea.nti.se>
2018-09-20 11:11:26 +0200

    gjorde det möjligt att logga in på hemsidan

--- test.php
+++ test.php
@@ -2,14 +2,16 @@
 include 'db.php';
 
 
-foreach ($dbh->query('SELECT * FROM login') as $row {
-	print_r($row);
+foreach ($dbh->query('SELECT * FROM login') as $row) {
+	echo "<pre>" . print_r($row,1) . "</pre>";
 }
 
+/*
 $statement = $dbh->query("SELECT * FROM login");
 $row = $statement->fetch(PDO::FETCH_ASSOC);
 
 var_dump($row);
 echo "<pre>" . print_r($row,1) . "</pre>";
+*/
 
 ?>
